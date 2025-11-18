a6551f969dbf3211cdc4b745ec07c1046a772efb
sunyoung <canimary123@gmail.com>
2025-11-18 17:16:39 +0900

    ch

---
+++ codes/05_ifs.py
@@ -0,0 +1,41 @@
+#실습 if 문
+
+# if condition:
+#     excuted Code
+# elif condtion2:
+#     excuted Code2
+# else:
+#     excuted Code
+
+
+
+
+# first = 4
+# # if first >= 5 :
+# #     print("welcome to the rollercoaster!")
+# #     print("it's me!")
+# second = 6
+# first = int(input("input first number: "))
+# second = int(input("input second number: "))
+# if first >= second :
+#     print("welcome to the rollercoaster!")
+#     print("it's me!")
+# print("End of program.")
+
+
+# ## 다중 if elif else 문
+
+
+
+# age = int(input("enter your age: "))  # 문자열을 정수로 변환
+# age = int(age)  # 문자열을 정수로 변환
+# if age < 4:
+#     print("your ticket is free")
+# elif age < 18:
+#     print("your ticket is $5.")
+# else:
+#     print("your ticket is $10.")
+
+# print("End of program.")
+
+pass
