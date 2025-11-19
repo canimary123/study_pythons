0faf259c43b49faf200244f8ff1d52c35327c75c
sunyoung <canimary123@gmail.com>
2025-11-19 13:10:53 +0900

    ch

---
+++ codes/08_functions.py
@@ -0,0 +1,48 @@
+
+# temp1 = 77
+# celsius1 = (temp1 - 32) * 5 / 9
+# print(celsius1)
+# temp2 = 95
+# celsius2 = (temp2 - 32) * 5 / 9
+# print(celsius2)
+# temp3 = 50
+# celsius3 = (temp3 - 32) * 5 / 9
+# print(celsius3)
+
+# 변수재사용
+
+# temp1 = 77
+# celsius1 = (temp1 - 32) * 5 / 9
+# print(celsius1)
+# temp2 = 95
+# celsius2 = (temp2 - 32) * 5 / 9
+# print(celsius2)
+# temp3 = 50
+# celsius3 = (temp3 - 32) * 5 / 9
+# print(celsius3)
+
+#함수 사용
+
+# def function_name(praram_first,...param_last):
+#     # 실행할 코드
+#     return return_value
+
+# temp = 50
+# celsius = (temp - 3) * 5 / 9
+# print(celsius)
+
+def to_celsius(temp):
+    celsius = (temp - 3) * 5 / 9
+    return celsius
+
+pass
+to_celsius(120)
+temp1 =to_celsius(77)
+print(temp1)
+pass
+
+
+temp2 =100
+result2 =to_celsius(temp2)
+print(result2)
+pass
