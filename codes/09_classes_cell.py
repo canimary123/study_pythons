680b5b7b514d1e583ff78761ed9314713595672f
sunyoung <canimary123@gmail.com>
2025-11-21 14:20:36 +0900

    ch

---
+++ codes/09_classes_cell.py
@@ -0,0 +1,11 @@
+from codes.classes_fourcals import FOURCAL
+
+if __name__ == "__main__":
+    fourcal = FOURCAL()
+    print(f"3 + 5 = {fourcal.add(3.5)}")
+    print(f"10 - 4 = {fourcal.sub(10, 4)}")
+    print(f"7 * 8 = {fourcal.mul(7, 8)}")
+    print(f"20 / 5 = {fourcal.div(20, 5)}")  # Calculator 클래스 작성하시오.
+    
+    pass
+
---
+++ codes/classes_fourcals.py
@@ -0,0 +1,44 @@
+# Python function
+# class class_name():
+#     # 속성(변수)
+
+    #메서드(함수)
+
+
+
+
+#메서드(함수)
+
+class FOURCAL:
+
+    first = 0
+    second = 0
+    
+    def __init__(self):
+        self.first = 4
+        self.second = 6
+        pass
+    #메서드(함수)
+    
+    def add(self, a, b):
+        return a + b
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, a, b):
+        return a * b
+    
+    def div(self, a, b):
+        if b == 0:
+            return "0으로 나눌 수 없습니다."
+        return a / b
+
+if __name__ == "__main__":
+    fourcal = FOURCAL()
+    print(f"3 + 5 = {fourcal.add(3,5)}")
+    print(f"10 - 4 = {fourcal.sub(10,4)}")
+    print(f"7 * 8 = {fourcal.mul(7,8)}")
+    print(f"20 / 5 = {fourcal.div(20,5)}")# Calculator 클래스 작성하시오.
+
+    pass
